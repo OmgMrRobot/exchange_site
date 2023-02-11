3f367f22ae68fc62fd716a205be723f50644b5af
OmgMrRobot <kuzivladimir@ramler.ru>
2023-02-11 16:11:46 +0300

    first commit

---
+++ app/exchange_app/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+# Create your models here.
+class Exchange_app(models.Model):
+    pass
---
+++ app/exchange_app/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+import requests
+import logging
+from time import asctime
+
+
+logger = logging.getLogger(__name__) # __name__ - каждый логер именнованный
+
+def exchange(request):
+    response = requests.get(url='https://api.exchangerate-api.com/v4/latest/USD').json()
+    currencies = response.get("rates")
+
+    if request.method == 'GET':
+        context = {
+            'currencies': currencies
+        }
+
+        return render(request=request, template_name='exchange_app/index.html', context=context)
+
+    if request.method == 'POST':
+        from_amount = request.POST.get('from-amount')
+        from_curr = request.POST.get('from-curr')
+        to_curr = request.POST.get('to-curr')
+        logger.debug(f'{asctime()} from_amount = {from_amount} from_curr = {from_curr} to_curr = {to_curr}')
+        from_amount = float(from_amount) if from_amount.isdigit() else 0
+        converted_amount = round((currencies[to_curr] / currencies[from_curr]) * float(from_amount), 2)
+
+        context = {
+            'from_curr': from_curr,
+            'to_curr': to_curr,
+            'from_amount': from_amount,
+            'currencies': currencies,
+            'converted_amount': converted_amount
+        }
+
+        return render(request=request, template_name='exchange_app/index.html', context=context)
+
+
+
+
+
